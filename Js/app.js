8ba756d78a470bafdcf7457ab88016d3e83d02b2
ApoloByte <142189729+ApoloByte@users.noreply.github.com>
2023-09-11 23:24:43 -0300

    Adicionado Js
    Falta corrigir o JS e implementar as funções de alterações de forma correta

---
+++ Js/app.js
@@ -0,0 +1,7 @@
+var title = document.querySelector("#Titulo");
+
+title.innerHTML =;
+
+var title = document.querySelector("#TextoDescricao");
+
+title.innerHTML = "Somos a maior empresa de cafe do brasil.";
